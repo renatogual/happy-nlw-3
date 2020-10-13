bb52be8408e70c373890464e7715ceadba5fc4af
renatogual <renatogual@hotmail.com>
2020-10-13 07:40:48 -0300

    Criado landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/logo.svg';
+import { FiArrowRight } from 'react-icons/fi';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Curitiba</strong>
+          <span>Paraná</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
+    </div>  
   );
 }
 
