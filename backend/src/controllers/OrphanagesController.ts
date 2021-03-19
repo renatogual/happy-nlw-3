545662ea79564ecdb823183f3aad55ade1ad60d3
renatogual <renatogual@hotmail.com>
2021-03-18 21:25:28 -0300

    Criando rotas para listar um ou todos os orfanatos

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "../models/Orphanage";
+
+export default {
+  //Lista todos os orfanatos
+  async index(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return res.json(orphanages);
+  },
+
+  //Lista os detalhes de um orfanato
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return res.json(orphanage);
+  },
+
+  //Cria um orfanato
+  async create(req: Request, res: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = req.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return res.status(201).json(orphanage);
+  },
+};
