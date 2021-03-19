e2a0e9fc16f02cafb46631cc53e916291a9b85ab
renatogual <renatogual@hotmail.com>
2021-03-18 21:00:47 -0300

    Criando primeira rota para cadastrar os orfanatos sem imagem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,39 @@
-import express from 'express';
-import './database/connection';
+import express from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
+
+import "./database/connection";
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/orphanages", async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = req.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return res.json({ message: "Orfanato cadastrado com sucesso!" });
+});
 
 app.listen(3333);
