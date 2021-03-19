d948e6dc8acd06bed5343b6f0a9ac25b9a528d4b
renatogual <renatogual@hotmail.com>
2021-03-18 22:27:34 -0300

    Criando funcionalidade de upload de imagens na api

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -37,6 +37,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
+    const requestImages = req.files as Express.Multer.File[]; //Forçando tipagem
+    const images = requestImages.map((image) => {
+      return { path: image.filename };
+    });
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -45,6 +50,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     });
 
     await orphanagesRepository.save(orphanage);
