36b18b41dc9a996988b1ac791ff9701d27da1b27
renatogual <renatogual@hotmail.com>
2021-04-10 23:20:12 -0300

    Reeorganizado OrphanagesMap

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,41 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
 
-import mapMarker from "./src/images/map-marker.png";
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -25.4804975,
-          longitude: -49.2740054,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker
-          icon={mapMarker}
-          coordinate={{ latitude: -25.4804975, longitude: -49.2740054 }}
-        >
-          <Callout>
-            <Text>Lar das meninas</Text>
-          </Callout>
-        </Marker>
-      </MapView>
-    </View>
-  );
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
